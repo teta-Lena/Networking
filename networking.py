6c3431bce2e3c704c7b7647012f7a86b38dd6806
teta-Lena <75176692+teta-Lena@users.noreply.github.com>
2021-11-30 21:52:26 +0200

    Update networking.py
    Last commit

--- networking.py
+++ networking.py
@@ -1,13 +1,21 @@
 import smtplib
-
 sender = "tetalenaa@gmail.com"
-receiver = "gbaziramwabo@gmail.com"
+receiver = "tetalenaa@gmail.com"
 
 message = """From: From Person <tetalenaa@gmail.com>
-To:To Person <gbaziramwabo@gmail.com"
+To:To Person <tetalenaa@gmail.com"
 Subject: Networking Assignment
-Good Evening Sir 
-This is my assignment my name is Ishimwe Teta Lena
-
-
+This is my assignment.My name is Ishimwe Teta Lena
+ You can view my code on Github at https://github.com/teta-Lena/Networking.git
+ 
 """
+try:
+    smtpObj = smtplib.SMTP("smtp.gmail.com",587)
+    smtpObj.ehlo()
+    smtpObj.starttls()
+    smtpObj.ehlo()
+    smtpObj.login("tetalenaa@gmail.com","Teta@2005")
+    smtpObj.sendmail(sender,receiver,message)
+    print("Successfully sent mail")
+except smtplib.SMTPException:
+    print("Error unable to send mail")
