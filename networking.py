795b45a49e4d6961770759691b98081a2e1204f5
teta-Lena <75176692+teta-Lena@users.noreply.github.com>
2021-11-30 21:24:08 +0200

    Add files via upload

---
+++ networking.py
@@ -0,0 +1,13 @@
+import smtplib
+
+sender = "tetalenaa@gmail.com"
+receiver = "gbaziramwabo@gmail.com"
+
+message = """From: From Person <tetalenaa@gmail.com>
+To:To Person <gbaziramwabo@gmail.com"
+Subject: Networking Assignment
+Good Evening Sir 
+This is my assignment my name is Ishimwe Teta Lena
+
+
+"""
